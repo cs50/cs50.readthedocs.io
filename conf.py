541d30d4140682b5b77fd075ce8c64ddaf2caf06
David J. Malan <malan@harvard.edu>
2020-05-31 22:54:33 -0400

    display_version

--- conf.py
+++ conf.py
@@ -1,7 +1,7 @@
 extensions = [
     "recommonmark",
     #"sphinx.ext.autodoc",
-    #"sphinx_markdown_tables"
+    "sphinx_markdown_tables"
 ]
 
 html_static_path = ["_static"]
@@ -9,6 +9,7 @@ html_static_path = ["_static"]
 html_css_files = ["css/custom.css"]
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {
+    "display_version": False,
     "prev_next_buttons_location": False,
     "sticky_navigation": False
 }
