f89f75c7f52e2622c7cbae16e88a6ff004cf7078
aleschvart <139076321+aleschvart@users.noreply.github.com>
2023-07-14 19:00:10 -0300

    Create indoor3.py
    #Best to use your “indoor voice” sometimes, writing entirely in lowercase.
    Excercise program for Indoor Voice. CS50P Python course, Prof. Phd. David Malan. Student: Schvartz Ana María Alejandra.
    I created a function to change UPPER into lowercase

---
+++ indoor3.py
@@ -0,0 +1,9 @@
+#Best to use your “indoor voice” sometimes, writing entirely in lowercase.
+def main()
+x = str
+x = "voice indoor "
+x = (input ("What's x? "))
+if is_UPPER(x)
+    print("VOICE INDOOR")
+else
+    print("voice indoor")
